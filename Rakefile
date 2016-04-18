6a38b2ee403f14c43f47a7859c44e60d20e55b04
Ryan Davis <ryand@zenspider.com>
2016-04-18 14:35:14 -0800

    - Added example_dot_autotest.rb and rake rule to regenerate it. [git-p4: depot-paths = "//src/minitest-autotest/dev/": change = 10583]

--- Rakefile
+++ Rakefile
@@ -14,4 +14,48 @@ Hoe.spec "minitest-autotest" do
   dependency "minitest-server", "~> 1.0"
 end
 
+desc "update example_dot_autotest.rb with all possible constants"
+task :update do
+  system "p4 edit example_dot_autotest.rb"
+  File.open "example_dot_autotest.rb", "w" do |f|
+    f.puts "# -*- ruby -*-"
+    f.puts
+    Dir.chdir "lib" do
+      Dir["autotest/*.rb"].sort.each do |s|
+        next if s =~ /rails|discover/
+        f.puts "# require '#{s[0..-4]}'"
+      end
+    end
+
+    f.puts
+
+    Dir["lib/autotest/*.rb"].sort.each do |file|
+      file = File.read(file)
+      m = file[/module.*/].split(/ /).last rescue nil
+      next unless m
+
+      dirty = false
+
+      file.lines.grep(/def[^(]+=/).each do |setter|
+        dirty = true
+        setter = setter.sub(/^ *def self\./, '').sub(/\s*=\s*/, ' = ')
+        f.puts "# #{m}.#{setter}"
+      end
+
+      f.puts if dirty
+    end
+
+    File.foreach("lib/autotest.rb") do |line|
+      next unless line =~ /hook (:\w+)/
+      name = $1
+
+      f.puts "# Autotest.add_hook #{name} do |at|"
+      f.puts "#   ... do stuff for #{name} hook ..."
+      f.puts "# end"
+      f.puts
+    end
+  end
+  system "p4 diff -du example_dot_autotest.rb"
+end
+
 # vim: syntax=ruby
---
+++ example_dot_autotest.rb
@@ -0,0 +1,68 @@
+# -*- ruby -*-
+
+# require 'autotest/autoupdate'
+# require 'autotest/bundler'
+# require 'autotest/fixtures'
+# require 'autotest/isolate'
+# require 'autotest/migrate'
+# require 'autotest/once'
+# require 'autotest/preload'
+# require 'autotest/rcov'
+# require 'autotest/restart'
+# require 'autotest/timestamp'
+
+# Autotest::AutoUpdate.sleep_time = o
+# Autotest::AutoUpdate.update_cmd = o
+
+# Autotest::Isolate.dir = o
+
+# Autotest::Preload.glob = o
+
+# Autotest::RCov.command = o
+# Autotest::RCov.pattern = o
+# Autotest::RCov.options = o
+
+# Autotest.add_hook :initialize do |at|
+#   ... do stuff for :initialize hook ...
+# end
+
+# Autotest.add_hook :post_initialize do |at|
+#   ... do stuff for :post_initialize hook ...
+# end
+
+# Autotest.add_hook :all_good do |at|
+#   ... do stuff for :all_good hook ...
+# end
+
+# Autotest.add_hook :quit do |at|
+#   ... do stuff for :quit hook ...
+# end
+
+# Autotest.add_hook :run_command do |at|
+#   ... do stuff for :run_command hook ...
+# end
+
+# Autotest.add_hook :ran_command do |at|
+#   ... do stuff for :ran_command hook ...
+# end
+
+# Autotest.add_hook :interrupt do |at|
+#   ... do stuff for :interrupt hook ...
+# end
+
+# Autotest.add_hook :updated do |at|
+#   ... do stuff for :updated hook ...
+# end
+
+# Autotest.add_hook :all_good do |at|
+#   ... do stuff for :all_good hook ...
+# end
+
+# Autotest.add_hook :reset do |at|
+#   ... do stuff for :reset hook ...
+# end
+
+# Autotest.add_hook :waiting do |at|
+#   ... do stuff for :waiting hook ...
+# end
+
